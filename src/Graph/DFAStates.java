1f90d47ac16cda5a837845a9e742335d9e005395
Gregory A. Sandoval <greg@goop.com>
2019-09-15 14:15:56 -0700

    Add keywords;

--- src/Graph/DFAStates.java
+++ src/Graph/DFAStates.java
@@ -4,6 +4,7 @@ import Graph.Token.*;
 
 import static Graph.Node.FINAL_STATE;
 import static Graph.Node.NON_FINAL_STATE;
+import static Graph.Token.*;
 
 public class DFAStates {
   // DFA Grammar
@@ -14,9 +15,9 @@ public class DFAStates {
   public static final Node START = new NON_FINAL_STATE("START");
   public static final Node IDENTIFIER = new FINAL_STATE("IDENTIFIER", IdentifierToken::build);
 
-  public static final Node INTEGER = new FINAL_STATE("INTEGER", Token.IntegerToken::build);
+  public static final Node INTEGER = new FINAL_STATE("INTEGER", IntegerToken::build);
   public static final Node MAYBE_FLOAT = new NON_FINAL_STATE("MAYBE_FLOAT");
-  public static final Node FLOAT = new FINAL_STATE("FLOAT", Token.FloatToken::build);
+  public static final Node FLOAT = new FINAL_STATE("FLOAT", FloatToken::build);
 
   public static final Node OPENING_STRING = new NON_FINAL_STATE("OPENING_STRING");
   public static final Node STRING_CONTENTS = new NON_FINAL_STATE("STRING_CONTENTS");
@@ -26,42 +27,42 @@ public class DFAStates {
   public static final Node COMMENT = new FINAL_STATE("COMMENT", CommentToken::new);
 
   // Unpaired delimiters
-  public static final Node COMMA = new FINAL_STATE("COMMA", SymbolToken.COMMA);
-  public static final Node SEMI_COLON = new FINAL_STATE("SEMI_COLON", SymbolToken.SEMI_COLON);
+  public static final Node COMMA = new FINAL_STATE("COMMA", Tokens.COMMA);
+  public static final Node SEMI_COLON = new FINAL_STATE("SEMI_COLON", Tokens.SEMI_COLON);
 
   // Paired delimiters
-  public static final Node LESS_THAN = new FINAL_STATE("LESS_THAN", OperatorToken.LESS_THAN);
-  public static final Node GREATER_THAN = new FINAL_STATE("GREATER_THAN", OperatorToken.GREATER_THAN);
+  public static final Node LESS_THAN = new FINAL_STATE("LESS_THAN", Tokens.LESS_THAN);
+  public static final Node GREATER_THAN = new FINAL_STATE("GREATER_THAN", Tokens.GREATER_THAN);
 
-  public static final Node LEFT_BRACE = new FINAL_STATE("LEFT_BRACE", SymbolToken.LEFT_BRACE);
-  public static final Node RIGHT_BRACE = new FINAL_STATE("RIGHT_BRACE", SymbolToken.RIGHT_BRACE);
+  public static final Node LEFT_BRACE = new FINAL_STATE("LEFT_BRACE", Tokens.LEFT_BRACE);
+  public static final Node RIGHT_BRACE = new FINAL_STATE("RIGHT_BRACE", Tokens.RIGHT_BRACE);
 
-  public static final Node LEFT_BRACKET = new FINAL_STATE("LEFT_BRACKET", SymbolToken.LEFT_BRACKET);
-  public static final Node RIGHT_BRACKET = new FINAL_STATE("RIGHT_BRACKET", SymbolToken.RIGHT_BRACKET);
+  public static final Node LEFT_BRACKET = new FINAL_STATE("LEFT_BRACKET", Tokens.LEFT_BRACKET);
+  public static final Node RIGHT_BRACKET = new FINAL_STATE("RIGHT_BRACKET", Tokens.RIGHT_BRACKET);
 
-  public static final Node LEFT_PAREN = new FINAL_STATE("LEFT_PAREN", SymbolToken.LEFT_PAREN);
-  public static final Node RIGHT_PAREN = new FINAL_STATE("RIGHT_PAREN", SymbolToken.RIGHT_PAREN);
+  public static final Node LEFT_PAREN = new FINAL_STATE("LEFT_PAREN", Tokens.LEFT_PAREN);
+  public static final Node RIGHT_PAREN = new FINAL_STATE("RIGHT_PAREN", Tokens.RIGHT_PAREN);
 
   // Other punctuation tokens
-  public static final Node ASTERISK = new FINAL_STATE("ASTERISK", OperatorToken.ASTERISK);
-  public static final Node CARET = new FINAL_STATE("CARET", SymbolToken.CARET);
-  public static final Node COLON = new FINAL_STATE("COLON", SymbolToken.COLON);
-  public static final Node PERIOD = new FINAL_STATE("PERIOD", SymbolToken.PERIOD);
-  public static final Node EQUAL = new FINAL_STATE("EQUAL", OperatorToken.EQUAL);
-  public static final Node MINUS = new FINAL_STATE("MINUS", OperatorToken.MINUS);
-  public static final Node PLUS = new FINAL_STATE("PLUS", OperatorToken.PLUS);
-  public static final Node FORWARD_SLASH = new FINAL_STATE("FORWARD_SLASH", SymbolToken.FORWARD_SLASH);
-  public static final Node AND = new FINAL_STATE("AND", OperatorToken.AND);
-  public static final Node EXCLAMATION_MARK = new FINAL_STATE("EXCLAMATION_MARK", OperatorToken.EXCLAMATION_MARK);
+  public static final Node ASTERISK = new FINAL_STATE("ASTERISK", Tokens.ASTERISK);
+  public static final Node CARET = new FINAL_STATE("CARET", Tokens.CARET);
+  public static final Node COLON = new FINAL_STATE("COLON", Tokens.COLON);
+  public static final Node PERIOD = new FINAL_STATE("PERIOD", Tokens.PERIOD);
+  public static final Node EQUAL = new FINAL_STATE("EQUAL", Tokens.EQUAL);
+  public static final Node MINUS = new FINAL_STATE("MINUS", Tokens.MINUS);
+  public static final Node PLUS = new FINAL_STATE("PLUS", Tokens.PLUS);
+  public static final Node FORWARD_SLASH = new FINAL_STATE("FORWARD_SLASH", Tokens.FORWARD_SLASH);
+  public static final Node AND = new FINAL_STATE("AND", Tokens.AND);
+  public static final Node EXCLAMATION_MARK = new FINAL_STATE("EXCLAMATION_MARK", Tokens.EXCLAMATION_MARK);
 
   // Multi character operators
-  public static final Node OP_ARROW = new FINAL_STATE("OP_ARROW", OperatorToken.OP_ARROW);
-  public static final Node OP_EQUAL = new FINAL_STATE("OP_EQUAL", OperatorToken.OP_EQUAL);
-  public static final Node OP_NEGATE = new FINAL_STATE("OP_NEGATE", OperatorToken.OP_NEGATE);
-  public static final Node OP_LESS_THAN = new FINAL_STATE("OP_LESS_THAN", OperatorToken.OP_LESS_THAN);
-  public static final Node OP_GREATER_THAN = new FINAL_STATE("OP_GREATER_THAN", OperatorToken.OP_GREATER_THAN);
-  public static final Node OP_SHIFT_LEFT = new FINAL_STATE("OP_SHIFT_LEFT", OperatorToken.OP_SHIFT_LEFT);
-  public static final Node OP_SHIFT_RIGHT = new FINAL_STATE("OP_SHIFT_RIGHT", OperatorToken.OP_SHIFT_RIGHT);
+  public static final Node OP_ARROW = new FINAL_STATE("OP_ARROW", Tokens.OP_ARROW);
+  public static final Node OP_EQUAL = new FINAL_STATE("OP_EQUAL", Tokens.OP_EQUAL);
+  public static final Node OP_NEGATE = new FINAL_STATE("OP_NEGATE", Tokens.OP_NEGATE);
+  public static final Node OP_LESS_THAN = new FINAL_STATE("OP_LESS_THAN", Tokens.OP_LESS_THAN);
+  public static final Node OP_GREATER_THAN = new FINAL_STATE("OP_GREATER_THAN", Tokens.OP_GREATER_THAN);
+  public static final Node OP_SHIFT_LEFT = new FINAL_STATE("OP_SHIFT_LEFT", Tokens.OP_SHIFT_LEFT);
+  public static final Node OP_SHIFT_RIGHT = new FINAL_STATE("OP_SHIFT_RIGHT", Tokens.OP_SHIFT_RIGHT);
 
   private DFAStates() {
   }
--- src/Graph/Token.java
+++ src/Graph/Token.java
@@ -4,45 +4,88 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Token {
+  private static int UUID = 5;
   public final String str;
+  public final int ID;
 
   protected Token(String str) {
     this.str = str;
+    this.ID = ++UUID;
+  }
+
+  protected Token(String str, int UUID) {
+    this.str = str;
+    this.ID = UUID;
   }
 
   @Override
   public String toString() {
-    return "(Tok: <ID#> line= <line, pos> str = \"" + this.str + "\")";
+    return "(Tok: " + ID + " line= <line, pos> str = \"" + this.str + "\")";
   }
 
-  public final static class SymbolToken extends Token {
+  public final static class Tokens {
+    // Unpaired delimiters
     public static final SymbolToken COMMA = new SymbolToken(",");
     public static final SymbolToken SEMI_COLON = new SymbolToken(";");
+
+    // Padding for IDs
+    private static final IDOffsetToken SKIP_ID_8 = new IDOffsetToken();
+    private static final IDOffsetToken SKIP_ID_9 = new IDOffsetToken();
+
+    // Keywords
+    public static final KeywordToken PROG = KeywordToken.build("prog");
+    public static final KeywordToken MAIN = KeywordToken.build("main");
+    public static final KeywordToken FCN = KeywordToken.build("fcn");
+    public static final KeywordToken CLASS = KeywordToken.build("class");
+    private static final IDOffsetToken SKIP_ID_14 = new IDOffsetToken();
+    public static final KeywordToken FLOAT = KeywordToken.build("float");
+    public static final KeywordToken INT = KeywordToken.build("int");
+    public static final KeywordToken STRING = KeywordToken.build("string");
+    public static final KeywordToken IF = KeywordToken.build("if");
+    public static final KeywordToken ELSEIF = KeywordToken.build("elseif");
+    public static final KeywordToken ELSE = KeywordToken.build("else");
+    public static final KeywordToken WHILE = KeywordToken.build("while");
+    public static final KeywordToken INPUT = KeywordToken.build("input");
+    public static final KeywordToken PRINT = KeywordToken.build("print");
+    public static final KeywordToken NEW = KeywordToken.build("new");
+    public static final KeywordToken RETURN = KeywordToken.build("return");
+    public static final KeywordToken VAR = KeywordToken.build("var");
+
+    // Padding for IDs
+    private static final IDOffsetToken SKIP_ID_27 = new IDOffsetToken();
+    private static final IDOffsetToken SKIP_ID_28 = new IDOffsetToken();
+    private static final IDOffsetToken SKIP_ID_29 = new IDOffsetToken();
+    private static final IDOffsetToken SKIP_ID_30 = new IDOffsetToken();
+
+    // Paired delimiters
+    public static final OperatorToken LESS_THAN = new OperatorToken("<");
+    public static final OperatorToken GREATER_THAN = new OperatorToken(">");
     public static final SymbolToken LEFT_BRACE = new SymbolToken("{");
     public static final SymbolToken RIGHT_BRACE = new SymbolToken("}");
     public static final SymbolToken LEFT_BRACKET = new SymbolToken("[");
     public static final SymbolToken RIGHT_BRACKET = new SymbolToken("]");
     public static final SymbolToken LEFT_PAREN = new SymbolToken("(");
     public static final SymbolToken RIGHT_PAREN = new SymbolToken(")");
+
+    // Padding for IDs
+    private static final IDOffsetToken SKIP_ID_39 = new IDOffsetToken();
+    private static final IDOffsetToken SKIP_ID_40 = new IDOffsetToken();
+
+    // Other punctuation
+    public static final OperatorToken ASTERISK = new OperatorToken("*");
     public static final SymbolToken CARET = new SymbolToken("^");
     public static final SymbolToken COLON = new SymbolToken(":");
     public static final SymbolToken PERIOD = new SymbolToken(".");
-    public static final SymbolToken FORWARD_SLASH = new SymbolToken("/");
-
-    private SymbolToken(String str) {
-      super(str);
-    }
-  }
-
-  public static final class OperatorToken extends Token {
-    public static final OperatorToken LESS_THAN = new OperatorToken("<");
-    public static final OperatorToken GREATER_THAN = new OperatorToken(">");
-    public static final OperatorToken ASTERISK = new OperatorToken("*");
     public static final OperatorToken EQUAL = new OperatorToken("=");
     public static final OperatorToken MINUS = new OperatorToken("-");
     public static final OperatorToken PLUS = new OperatorToken("+");
+    public static final SymbolToken FORWARD_SLASH = new SymbolToken("/");
     public static final OperatorToken AND = new OperatorToken("&");
-    public static final OperatorToken EXCLAMATION_MARK = new OperatorToken("!");
+
+    // Padding for IDs
+    private static final IDOffsetToken SKIP_ID_50 = new IDOffsetToken();
+
+    // Multi-char operators
     public static final OperatorToken OP_ARROW = new OperatorToken("->");
     public static final OperatorToken OP_EQUAL = new OperatorToken("==");
     public static final OperatorToken OP_NEGATE = new OperatorToken("!=");
@@ -51,14 +94,43 @@ public abstract class Token {
     public static final OperatorToken OP_SHIFT_LEFT = new OperatorToken("<<");
     public static final OperatorToken OP_SHIFT_RIGHT = new OperatorToken(">>");
 
+
+    public static final OperatorToken EXCLAMATION_MARK = new OperatorToken("!");
+  }
+
+  public final static class SymbolToken extends Token {
+    private SymbolToken(String str) {
+      super(str);
+    }
+  }
+
+  private final static class IDOffsetToken extends Token {
+    private IDOffsetToken() {
+      super("This should not be printed; Error!");
+    }
+  }
+
+  public static final class OperatorToken extends Token {
+
     protected OperatorToken(String str) {
       super(str);
     }
   }
 
   public static final class KeywordToken extends Token {
-    protected KeywordToken(String str) {
+    private static Map<String, KeywordToken> identityMap = new HashMap<>();
+
+    private KeywordToken(String str) {
       super(str);
+    }
+
+    public static KeywordToken build(String string) {
+      return identityMap.computeIfAbsent(string, KeywordToken::new);
+    }
+
+    public static Token get(Token identifier) {
+      var cached = identityMap.get(identifier.str);
+      return (cached == null) ? identifier : cached;
     }
   }
 
@@ -66,7 +138,7 @@ public abstract class Token {
     private static Map<String, StringToken> identityMap = new HashMap<>();
 
     private StringToken(String str) {
-      super(str.replace("\"", ""));
+      super(str.replace("\"", ""), 5);
     }
 
     public static StringToken build(String string) {
@@ -76,32 +148,32 @@ public abstract class Token {
 
   public static final class WhitespaceToken extends Token {
     protected WhitespaceToken(String str) {
-      super(str);
+      super(str, 100);
     }
   }
 
   public static final class CommentToken extends Token {
     protected CommentToken(String str) {
-      super(str);
+      super(str, 1);
     }
   }
 
   public static abstract class TypedToken<Value> extends Token {
     public final Value value;
 
-    protected TypedToken(String str) {
-      super(str);
+    protected TypedToken(String str, int tokenID) {
+      super(str, tokenID);
       this.value = parse(str);
     }
 
     protected abstract Value parse(String str);
   }
 
-  public static final class IdentifierToken extends TypedToken<String> {
+  public static class IdentifierToken extends TypedToken<String> {
     private static Map<String, IdentifierToken> identityMap = new HashMap<>();
 
     public IdentifierToken(String s) {
-      super(s);
+      super(s, 2);
     }
 
     @Override
@@ -118,7 +190,7 @@ public abstract class Token {
     private static Map<String, FloatToken> identityMap = new HashMap<>();
 
     protected FloatToken(String str) {
-      super(str);
+      super(str, 4);
     }
 
     public static FloatToken build(String string) {
@@ -132,7 +204,7 @@ public abstract class Token {
 
     @Override
     public String toString() {
-      return "(Tok: <ID#> line= <line, pos> str = \"" + this.str + "\" flo= " + this.value + ")";
+      return "(Tok: " + ID + " line= <line, pos> str = \"" + this.str + "\" flo= " + this.value + ")";
     }
   }
 
@@ -140,7 +212,7 @@ public abstract class Token {
     private static Map<String, IntegerToken> identityMap = new HashMap<>();
 
     protected IntegerToken(String str) {
-      super(str);
+      super(str, 3);
     }
 
     public static IntegerToken build(String string) {
@@ -151,9 +223,10 @@ public abstract class Token {
     protected Integer parse(String str) {
       return Integer.parseInt(str);
     }
+
     @Override
     public String toString() {
-      return "(Tok: <ID#> line= <line, pos> str = \"" + this.str + "\" int= " + this.value + ")";
+      return "(Tok: " + ID + " line= <line, pos> str = \"" + this.str + "\" int= " + this.value + ")";
     }
   }
 }
