0a30b2422839fbc00c4b9594d6b1e65c5e1d6326
Gregory A. Sandoval <greg@goop.com>
2019-09-15 21:22:00 -0700

    Final adjustments;

--- src/compiler/lexer/NonFinalState.java
+++ src/compiler/lexer/NonFinalState.java
@@ -1,10 +1,8 @@
 package compiler.lexer;
 
-import compiler.graph.Node;
-
 public class NonFinalState extends LexicalNode {
-  public static final Node END_OF_TERMINAL = new NonFinalState("END_OF_TERMINAL");
-  public static final Node FATAL_ERROR = new NonFinalState("FATAL_ERROR");
+  public static final NonFinalState END_OF_TERMINAL = new NonFinalState("END_OF_TERMINAL");
+  public static final NonFinalState FATAL_ERROR = new NonFinalState("FATAL_ERROR");
 
   public NonFinalState(String name) {
     super(name);
--- src/compiler/utils/TextCursor.java
+++ src/compiler/utils/TextCursor.java
@@ -10,7 +10,7 @@ public class TextCursor implements Iterator<Character>, Iterable<Character> {
   private int cursor = -1;
 
   public TextCursor(@NotNull String text) {
-    this.text = (text + "\n\n").toCharArray();
+    this.text = (text + " ").toCharArray();
   }
 
   @Override
